64dc333798d7a33552667f86def34db1e7cb9ef8
volkan <volkancaliskan14@gmail.com>
2022-04-20 06:02:25 +0300

    update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,55 +1,38 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
-const FeatureList = [
-
+export const FeatureList = [
   {
-    Svg: () => (
-      <svg width="50%" height="50%" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M25.5 0C11.2 0 0 11.2 0 25.5C0 39.8 11.2 50 25.5 50C39.8 50 50 39.8 50 25.5C50 11.2 39.8 0 25.5 0ZM25.5 1.5C12.8 1.5 1.5 12.8 1.5 25.5C1.5 38.2 12.8 49.5 25.5 49.5C38.2 49.5 49.5 38.2 49.5 25.5C49.5 12.8 38.2 1.5 25.5 1.5Z" fill="#FFC107"/>
-    </svg>
-    ),
-    title: 'Easy to use',
-    description: 'We have a simple and easy to use API to create your own custom components.'
+    title: '📝 Documentation',
+    description: 'Documentation for the project.',
   },
   {
-    Svg: () => (
-      <svg width="50%" height="50%" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M25.5 0C11.2 0 0 11.2 0 25.5C0 39.8 11.2 50 25.5 50C39.8 50 50 39.8 50 25.5C50 11.2 39.8 0 25.5 0ZM25.5 1.5C12.8 1.5 1.5 12.8 1.5 25.5C1.5 38.2 12.8 49.5 25.5 49.5C38.2 49.5 49.5 38.2 49.5 25.5C49.5 12.8 38.2 1.5 25.5 1.5Z" fill="#FFC107" />
-      </svg>
-    ),
-    title: 'Easy to use',
-    description: 'We have a simple and easy to use API to create your own custom components.'
+    title: '💻 Development',
+    description: 'Development tools for the project.',
   },
   {
-    Svg: () => (
-      <svg width="50%" height="50%" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M25.5 0C11.2 0 0 11.2 0 25.5C0 39.8 11.2 50 25.5 50C39.8 50 50 39.8 50 25.5C50 11.2 39.8 0 25.5 0ZM25.5 1.5C12.8 1.5 1.5 12.8 1.5 25.5C1.5 38.2 12.8 49.5 25.5 49.5C38.2 49.5 49.5 38.2 49.5 25.5C49.5 12.8 38.2 1.5 25.5 1.5Z" fill="#FFC107" />
-      </svg>
-    ),
-    title: 'Easy to use',
-    description: 'We have a simple and easy to use API to create your own custom components.'
+    title: '🚀 Launch',
+    description: 'Launch the project.',
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
+    <div className={clsx('col col--4', styles.feature, 'text--center')}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <Link className={(styles.link, 'button button--primary button--lg')} to="/docs/intro">
+        Learn more
+      </Link>
     </div>
   );
 }
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
+    <div className={styles.features}>
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
@@ -57,6 +40,7 @@ export default function HomepageFeatures() {
           ))}
         </div>
       </div>
-    </section>
+    </div>
   );
 }
+
